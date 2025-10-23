95f00b7b5dd68e1e6bd86433aaf95782f935fa8a
nevada <natalina686@gmail.com>
2025-10-23 17:53:04 +0200

    DP

---
+++ dp.py
@@ -0,0 +1,49 @@
+#top-down
+def fib_memo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fib_memo(n - 1, memo) + fib_memo(n - 2, memo)
+    return memo[n]
+
+#bottom-up
+def fib_dp(n):
+    dp = [0, 1]
+    for i in range(2, n + 1):
+        dp.append(dp[i-1] + dp[i-2])
+    return dp[n]
+
+def min_steps(n):
+    dp = [0] * (n+1)
+    dp[1] = 0
+
+    for i in range(2, n+1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    return dp[n]
+
+print(min_steps(100))
+
+def stairs(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+print(stairs(45))
+
+
