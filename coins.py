ed795c0bdc8f98318f18f4fa701c75c9ddf3e578
nevada <natalina686@gmail.com>
2025-10-25 14:00:26 +0200

    dp/greedy

--- coins.py
+++ coins.py
@@ -10,14 +10,22 @@ for coin in sorted(coins, reverse=True):
 print(result)
 
 another_coins = [1, 3, 4]
-sum = 6
+total = 6
 result_2 = []
 
 for another_coin in sorted(another_coins, reverse=True):
-    while sum >= another_coin:
-        sum -= another_coin
+    while total >= another_coin:
+        total -= another_coin
         result_2.append(another_coin)
+        optimal = 2
 print(result_2)
+
+
+optimal = 2
+if len(result_2) != optimal:
+    print("Greedy failed - not optimal")
+else:
+    print("Greedy works optimally ✅")
 
 
 def greedy_coin_change(coins, amount):
@@ -29,6 +37,35 @@ def greedy_coin_change(coins, amount):
                result.append(coin)
     
     return result, len(result)
+
+def dp_coin_change(coins, amount):
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[amount] if dp[amount] != float('inf') else -1
+
+def compare_algorithms(coins, amount):
+    greedy_result, greedy_count = greedy_coin_change(coins,amount)
+    dp_count = dp_coin_change(coins, amount)
+
+    print(f"Монети: {coins}, сума: {amount}")
+    print(f"Greedy: {greedy_result} (кількість = {greedy_count})")
+    print(f"Оптимальне (DP): {dp_count} монет")
+
+    if greedy_count == dp_count:
+        print("Greedy знайшов оптимальне рішення!")
+    else:
+        print("Greedy НЕ оптимальне.")
+
+
+
+compare_algorithms([1, 2, 5], 11)
+print()
+compare_algorithms([1, 3, 4], 6)
 
 coins = [1, 2, 5, 10, 20, 50]
 amount = 93
