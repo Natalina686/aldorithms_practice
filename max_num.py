84c9f997398b9639cf4fa3a9fe3937f530aa4b85
nevada <natalina686@gmail.com>
2025-10-27 19:59:19 +0100

    max_profit

--- max_num.py
+++ max_num.py
@@ -8,3 +8,16 @@ for number in numbers:
 
 print(max_number)
 
+prices = [7, 1, 5, 3, 6, 4]
+
+min_price = prices[0]
+max_profit = 0
+
+for price in prices:
+    profit = price - min_price
+    if profit > max_profit:
+        max_profit = profit
+    if price < min_price:
+        min_price = price
+
+print(max_profit)
